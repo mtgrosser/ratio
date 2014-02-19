09e5ee717549da5f088f42e0e242bd615cc106dc
Matthias Grosser <mtgrosser@gmx.net>
2014-02-19 18:06:20 +0100

    Fix delegation

--- lib/ratio.rb
+++ lib/ratio.rb
@@ -2,7 +2,7 @@ require 'rational'
 require 'forwardable'
 
 class Ratio < Numeric
-  VERSION = '1.0.0'
+  VERSION = '1.0.1'
   
   extend Forwardable
   
@@ -34,7 +34,8 @@ class Ratio < Numeric
   end
   
   include InstanceMethods
-  def_instance_delegators :to_r, *(Ratio.instance_methods - InstanceMethods.instance_methods - Object.instance_methods + [:==])
+  
+  def_instance_delegators :to_r, *(Rational.instance_methods - InstanceMethods.instance_methods - Object.instance_methods + [:==])
 end
 
 module Kernel
@@ -43,18 +44,4 @@ module Kernel
   def Ratio(numerator, denominator)
     Ratio.new(numerator, denominator)
   end
-end
-
-
-module Enumerable
-  
-  def each_with_progress(&block)
-    enum_size = count
-    if block_given?
-      each_with_index { |obj, idx| yield(obj, Ratio(idx + 1, enum_size)) }
-    else
-      enum_for :each_with_progress
-    end
-  end
-  
 end
--- test/ratio_test.rb
+++ test/ratio_test.rb
@@ -2,27 +2,40 @@ require_relative 'test_helper'
 
 class RatioTest < MiniTest::Unit::TestCase
   
-  def test_ratio
+  def test_instantiator
     assert_kind_of Ratio, Ratio(1, 2)
     assert_equal 0.5, Ratio(1, 2)
     assert_equal 1,   Ratio(101, 101)
     assert_equal 0,   Ratio(0, 10)
-    assert_raises(ZeroDivisionError) { Ratio(1, 0) }
-    assert_equal 100, Ratio(100, 100).numerator
-    assert_equal 100, Ratio(100, 100).denominator
   end
-
-  def test_each_with_progress
-    assert_equal [[1, Ratio(1, 10)],
-                  [2, Ratio(2, 10)],
-                  [3, Ratio(3, 10)],
-                  [4, Ratio(4, 10)],
-                  [5, Ratio(5, 10)],
-                  [6, Ratio(6, 10)],
-                  [7, Ratio(7, 10)],
-                  [8, Ratio(8, 10)],
-                  [9, Ratio(9, 10)],
-                  [10, Ratio(10, 10)]], (1..10).each_with_progress.to_a
+  
+  def test_zero_denominator_raises_zero_division_error
+    assert_raises(ZeroDivisionError) { Ratio(1, 0) }
+  end
+  
+  def test_numerator_and_denominator
+    assert_equal 200, Ratio(200, 100).numerator
+    assert_equal 100, Ratio(200, 100).denominator
+  end
+  
+  def test_addition
+    assert_equal Ratio(5, 4), Ratio(1, 2) + Ratio(3, 4)
+  end
+  
+  def test_subtraction
+    assert_equal Ratio(1, 6), Ratio(1, 2) - Ratio(1, 3)
+  end
+  
+  def test_multiplication
+    assert_equal Ratio(1, 4), Ratio(1, 2) * Ratio(1, 2)
+  end
+  
+  def test_division
+    assert_equal Ratio(1, 1), Ratio(1, 2) / Ratio(1, 2)
+  end
+  
+  def test_exponentiation
+    assert_equal Ratio(1, 8), Ratio(1, 2) ** 3
   end
   
 end
