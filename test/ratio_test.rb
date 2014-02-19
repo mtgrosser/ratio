4adefa5aadbe4689733fb74aec63d2d0df0d9177
Matthias Grosser <mtgrosser@gmx.net>
2014-02-19 15:49:27 +0100

    Adding test for each_with_progress

--- test/ratio_test.rb
+++ test/ratio_test.rb
@@ -11,5 +11,18 @@ class RatioTest < MiniTest::Unit::TestCase
     assert_equal 100, Ratio(100, 100).numerator
     assert_equal 100, Ratio(100, 100).denominator
   end
+
+  def test_each_with_progress
+    assert_equal [[1, Ratio(1, 10)],
+                  [2, Ratio(2, 10)],
+                  [3, Ratio(3, 10)],
+                  [4, Ratio(4, 10)],
+                  [5, Ratio(5, 10)],
+                  [6, Ratio(6, 10)],
+                  [7, Ratio(7, 10)],
+                  [8, Ratio(8, 10)],
+                  [9, Ratio(9, 10)],
+                  [10, Ratio(10, 10)]], (1..10).each_with_progress.to_a
+  end
   
 end
